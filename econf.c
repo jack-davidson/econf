ec6e89f0af4992e95ce957c67ec2d8e15e2917b0
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-05-30 18:17:06 -0500

    formatted code

--- econf.c
+++ econf.c
@@ -1,19 +1,20 @@
+#define  _XOPEN_SOURCE 700
+#include <dirent.h>
 #include <stdio.h>
 #include <string.h>
-#include <wordexp.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <dirent.h>
 #include <sys/stat.h>
+#include <wordexp.h>
+
+#define DOT_OR_DOTDOT(name) strcmp(name, "..") || strcmp(name, ".")
 
 #define VERSION "0.3"
 
 #define PATH_SIZE	 1024 /* maximum supported path size */
-
 #define LINE_BUFFER_SIZE 1024 /* maximum length of line in config file */
 #define TOKEN_SIZE	 256  /* maximum length of a config token */
 #define TOKENS		 128  /* maximum allowed config tokens */
-
 #define HOSTNAME_SIZE	 48   /* maximum size of hostname */
 #define COMMAND_SIZE	 512  /* maximum size of a command */
 
@@ -34,29 +35,26 @@ void version();
 int rm(char *path)
 {
 	struct stat path_stat;
+	struct dirent *entry;
+	DIR *src_dir;
+	char new_path[PATH_SIZE];
 	int is_dir;
 
 	lstat(path, &path_stat);
-
 	is_dir = S_ISDIR(path_stat.st_mode);
 
 	if (is_dir) {
-		DIR *src_dir;
-		struct dirent *entry;
-
 		src_dir = opendir(path);
 		if (src_dir == NULL){
 			fprintf(stderr, "cannot open %s\n", path);
 			return 1;
 		}
 		while ((entry = readdir(src_dir)) != NULL) {
-			if (strcmp(entry->d_name, "..") && strcmp(entry->d_name, ".")) {
-				char *new_path;
-
-				new_path = malloc(PATH_SIZE);
-				snprintf(new_path, PATH_SIZE, "%s/%s", path, entry->d_name);
+			if (DOT_OR_DOTDOT(entry->d_name)) {
+				snprintf(new_path, PATH_SIZE,
+					 "%s/%s", path,
+					 entry->d_name);
 				rm(new_path);
-				free(new_path);
 			}
 		}
 		closedir(src_dir);
@@ -68,31 +66,29 @@ int rm(char *path)
 /* link a directory src to another directory dest */
 int link_dir(char *dest, char *src)
 {
-	char *src_dir;
-	char *dest_dir;
+	char src_dir[PATH_SIZE];
+	char dest_dir[PATH_SIZE];
 	char cwd[PATH_SIZE];
 
 	if(access(src, F_OK)) {
 		fprintf(stderr, "cannot open directory: %s\n", src);
-		return 1;
+		return -1;
 	}
 
-	src_dir = malloc(PATH_SIZE);
-	dest_dir = malloc(PATH_SIZE);
-
 	getcwd(cwd, PATH_SIZE);
-	snprintf(src_dir, PATH_SIZE, "%s/%s", cwd, src);
-	snprintf(dest_dir, PATH_SIZE, "%s/%s", dest, src);
-
+	snprintf(src_dir, PATH_SIZE,
+		 "%s/%s", cwd, src);
+	snprintf(dest_dir, PATH_SIZE,
+		 "%s/%s", dest, src);
 	rm(dest_dir);
 
 	if(!symlink(src_dir, dest_dir))
-		fprintf(stdout, "%s -> %s\n", src_dir, dest_dir);
+		fprintf(stdout, "%s -> %s\n",
+			src_dir, dest_dir);
 	else
-		fprintf(stderr, "symlink failed: %s -> %s\n", src_dir, dest_dir);
+		fprintf(stderr, "symlink failed: %s -> %s\n",
+			src_dir, dest_dir);
 
-	free(dest_dir);
-	free(src_dir);
 	return 0;
 }
 
@@ -101,6 +97,9 @@ int link_dotfiles(char *dest, char *src)
 {
 	DIR *src_dir;
 	struct dirent *entry;
+	char dest_filename[PATH_SIZE];
+	char src_filename[PATH_SIZE];
+	char cwd[PATH_SIZE];
 
 	src_dir = opendir(src);
 	if (src_dir == NULL){
@@ -111,26 +110,22 @@ int link_dotfiles(char *dest, char *src)
 	while ((entry = readdir(src_dir)) != NULL) {
 		/* Exclude '.' and '..' files from source directory */
 		if (strcmp(entry->d_name, "..") && strcmp(entry->d_name, ".")) {
-			char *dest_filename;
-			char *src_filename;
-			char cwd[PATH_SIZE];
-
-			dest_filename = malloc(PATH_SIZE);
-			src_filename = malloc(PATH_SIZE);
-
 			getcwd(cwd, PATH_SIZE);
-			snprintf(dest_filename, PATH_SIZE, "%s/%s%s", dest, ".", entry->d_name);
-			snprintf(src_filename, PATH_SIZE, "%s/%s/%s", cwd, src, entry->d_name);
-
+			snprintf(dest_filename,
+				 PATH_SIZE,
+				 "%s/%s%s", dest,
+				 ".", entry->d_name);
+			snprintf(src_filename, PATH_SIZE,
+				 "%s/%s/%s", cwd, src,
+				 entry->d_name);
 			rm(dest_filename);
-
 			if (!symlink(src_filename, dest_filename))
-				fprintf(stdout, "%s -> %s\n", src_filename, dest_filename);
+				fprintf(stdout, "%s -> %s\n",
+					src_filename, dest_filename);
 			else
-				fprintf(stderr, "symlink failed: %s -> %s\n", src_filename, dest_filename);
-
-			free(dest_filename);
-			free(src_filename);
+				fprintf(stderr,
+					"symlink failed: %s -> %s\n",
+					src_filename, dest_filename);
 		}
 	}
 
@@ -156,6 +151,7 @@ int parse_args(int argc, char *argv[]) {
 		usage();
 	if (argc > 1 && !strcmp(argv[1], "-v"))
 		version();
+	return 0;
 }
 
 /* load config file, handle errors and return file descriptor */
@@ -174,10 +170,12 @@ FILE *load_config(char *config_filename)
 /* parse tokens of config line, called by lex_config for each line of config file */
 int parse_config(char line_tokens[TOKENS][TOKEN_SIZE], int token_index)
 {
+	char hostname[HOSTNAME_SIZE];
+	char command[COMMAND_SIZE];
+	int i;
+
 	if (token_index > 3) {
 		if (!strcmp(line_tokens[3], "sys")) {
-			char hostname[HOSTNAME_SIZE];
-
 			gethostname(hostname, HOSTNAME_SIZE);
 			strcat(line_tokens[1], "-");
 			strcat(line_tokens[1], hostname);
@@ -192,9 +190,8 @@ int parse_config(char line_tokens[TOKENS][TOKEN_SIZE], int token_index)
 			link_dotfiles(line_tokens[2], line_tokens[1]);
 		}
 		if (!strcmp(line_tokens[0], "hook")) {
-			char command[COMMAND_SIZE] = {0};
-
-			for (int i = 1; i < token_index; i++) {
+			memset(command, 0, COMMAND_SIZE);
+			for (i = 1; i < token_index; i++) {
 				strcat(command, line_tokens[i]);
 				strcat(command, " ");
 			}
@@ -208,17 +205,15 @@ int parse_config(char line_tokens[TOKENS][TOKEN_SIZE], int token_index)
 int lex_config(FILE *config)
 {
 	char line_buffer[LINE_BUFFER_SIZE];
+	char line_tokens[TOKENS][TOKEN_SIZE];
+	char new_token[TOKEN_SIZE];
+	int token_index;
+	char *token;
 	int line;
 	int err;
 
 	line = 1;
-
 	while ((fgets(line_buffer, LINE_BUFFER_SIZE, config) != NULL)) {
-		char line_tokens[TOKENS][TOKEN_SIZE];
-		char *token;
-		int token_index;
-
-		/* ignore lines starting with newline or comment symbol */
 		switch (line_buffer[0]) {
 			case '\n':
 				continue;
@@ -226,25 +221,22 @@ int lex_config(FILE *config)
 				fgets(line_buffer, LINE_BUFFER_SIZE, config);
 				continue;
 		}
-
-		/* remove trailing newline */
 		line_buffer[strcspn(line_buffer, "\n")] = 0;
 
-		/* tokenize config, filter input */
 		token_index = 0;
 		token = strtok(line_buffer, " ");
 		while (token != NULL) {
 			/* expand ~ to $HOME */
 			if (token[0] == '~') {
-				char new_token[TOKEN_SIZE];
-
-				memmove(token, token+1, strlen(token)); /* remove first char in token */
-				snprintf(new_token, TOKEN_SIZE, "%s%s", getenv("HOME"), token);
+				memmove(token, token+1, strlen(token));
+				snprintf(new_token,
+					 TOKEN_SIZE,
+					 "%s%s",
+					 getenv("HOME"),
+					 token);
 				strcpy(line_tokens[token_index], new_token);
-			/* handle comments */
 			} else if (token[0] == '#') {
 				break;
-			/* otherwise copy the token to line_tokens */
 			} else {
 				strcpy(line_tokens[token_index], token);
 			}
@@ -252,8 +244,9 @@ int lex_config(FILE *config)
 			token_index++;
 		}
 
-		if (err = (parse_config(line_tokens, token_index)) != 0) {
-			printf("failed to execute line %i in config (error code %i)\n", line, err);
+		if ((err = (parse_config(line_tokens, token_index))) < 0) {
+			printf("failed to execute line %i in config (error code %i)\n",
+			       line, err);
 			return err;
 		}
 
