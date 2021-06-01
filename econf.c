94adb329635570d593f3abe3a23338a02ea9fe7a
Jack Davidson <jackmaverickdavidson@gmail.com>
2021-06-01 09:46:17 -0500

    update econf.c

--- econf.c
+++ econf.c
@@ -28,7 +28,9 @@
 #define HOSTNAME_SIZE	 48   /* maximum size of hostname */
 #define COMMAND_SIZE	 512  /* maximum size of a command */
 
-int parseline(char line_tokens[TOKENS][TOKEN_SIZE], int token_index);
+int parseline(char tokens[TOKENS][TOKEN_SIZE], int t);
+char *stripnewline(char *s);
+char *expandtilde(char *dest, char *src, int l);
 int parseargs(int argc, char **argv);
 int linkfiles(char *dest, char *src);
 int linkdir(char *dest, char *src);
@@ -61,16 +63,16 @@ char *strncomb(char *s, size_t n, ...)
 /* recursively delete a directory or file */
 int rm(char *path)
 {
-	char new_path[PATH_SIZE];
 	struct stat path_stat;
-	struct dirent *entry;
-	DIR *src_dir;
 	int is_dir;
 
 	lstat(path, &path_stat);
 	is_dir = S_ISDIR(path_stat.st_mode);
 
 	if (is_dir) {
+		struct dirent *entry;
+		DIR *src_dir;
+
 		src_dir = opendir(path);
 		if (src_dir == NULL){
 			fprintf(stderr, "cannot open %s\n", path);
@@ -78,7 +80,9 @@ int rm(char *path)
 		}
 		while ((entry = readdir(src_dir)) != NULL) {
 			if (DOT_OR_DOTDOT(entry->d_name)) {
-				strncomb(new_path, PATH_SIZE - 1, path, "/", entry->d_name, NULL);
+				char new_path[PATH_SIZE];
+				strncomb(new_path, PATH_SIZE - 1,
+				       	 path, "/", entry->d_name, NULL);
 				rm(new_path);
 			}
 		}
@@ -140,8 +144,10 @@ int link_dotfiles(char *dest, char *src)
 			memset(dest_filename, 0, PATH_SIZE);
 			memset(src_filename, 0, PATH_SIZE);
 
-			strncomb(dest_filename, PATH_SIZE - 1, dest, "/.", entry->d_name, NULL);
-			strncomb(src_filename, PATH_SIZE - 1, cwd, "/", src, "/", entry->d_name, NULL);
+			strncomb(dest_filename, PATH_SIZE - 1,
+				 dest, "/.", entry->d_name, NULL);
+			strncomb(src_filename, PATH_SIZE - 1,
+				 cwd, "/", src, "/", entry->d_name, NULL);
 
 			rm(dest_filename);
 			if (!symlink(src_filename, dest_filename))
@@ -179,9 +185,12 @@ int strtolower(char *s, char *str, size_t size)
 
 int install(char *script)
 {
-	char cwd[PATH_SIZE];
 	char script_path[PATH_SIZE];
+	char cwd[PATH_SIZE];
 	char confirm[10];
+
+	memset(script_path, 0, sizeof(script_path));
+	memset(confirm, 0, sizeof(confirm));
 
 	getcwd(cwd, PATH_SIZE);
 	strncomb(script_path, PATH_SIZE - 1, cwd, "/install/", script, NULL);
@@ -189,6 +198,7 @@ int install(char *script)
 	printf("install %s? [Y/n] ", script);
 	scanf("%c", confirm);
 	strtolower(confirm, confirm, sizeof(confirm));
+
 	if (strcmp(confirm, "n")) {
 		printf("executing install script %s\n", script);
 		if (execl(script_path, "", NULL) < 0) {
@@ -203,7 +213,7 @@ int parseargs(int argc, char **argv) {
 	int option;
 
 	while ((option = getopt(argc, argv, "vhC:")) != -1) {
-	switch (option) {
+		switch (option) {
 		case 'v':
 			version();
 			exit(0);
@@ -217,7 +227,7 @@ int parseargs(int argc, char **argv) {
 			break;
 		default:
 			break;
-	}
+		}
 	}
 
 	if (argc > optind && !strcmp(argv[optind], "install"))
@@ -228,62 +238,97 @@ int parseargs(int argc, char **argv) {
 /* load config file, handle errors and return file descriptor */
 FILE * loadfile(char *file)
 {
-	FILE *fd;
-
 	if (!access(file, F_OK)) {
+		FILE *fd;
 		fd = fopen(file, "r");
 		return fd;
 	}
 	return NULL;
 }
 
-/* parse tokens of config line, called by readconfig for each line of config file */
-int parseline(char line_tokens[TOKENS][TOKEN_SIZE], int token_index)
+/* run command using argv with size TOKENS and each element size TOKEN_SIZE, and argc */
+int command(char tokens[TOKENS][TOKEN_SIZE], int t)
 {
-	char hostname[HOSTNAME_SIZE];
-	char command[COMMAND_SIZE];
+	char command[COMMAND_SIZE] = {0};
 	int i;
 
-	if (line_tokens[1][strlen(line_tokens[1]) - 1] == '@') {
-		line_tokens[1][strlen(line_tokens[1]) - 1] = 0;
+	for (i = 1; i < t; i++) {
+		strncomb(command, COMMAND_SIZE - 1, tokens[i], " ", NULL);
+	}
+	system(command);
+	return 0;
+}
+
+/* parse tokens of config line, called by readconfig for each line of config file
+ *
+ * tokens: array of strings containing tokens
+ * t:      amount of tokens
+ */
+int parseline(char tokens[TOKENS][TOKEN_SIZE], int t)
+{
+	if (tokens[1][strlen(tokens[1]) - 1] == '@') {
+		char hostname[HOSTNAME_SIZE];
+		tokens[1][strlen(tokens[1]) - 1] = 0;
 		gethostname(hostname, HOSTNAME_SIZE);
-		strncomb(line_tokens[1], TOKEN_SIZE - 1, "-", hostname, NULL);
+		strncomb(tokens[1], TOKEN_SIZE - 1, "-", hostname, NULL);
 	}
 
-	if (token_index >= 2) {
-		if (!strcmp(line_tokens[0], "dir")) {
-			linkdir(line_tokens[2], line_tokens[1]);
+	if (t >= 2) {
+		if (!strcmp(tokens[0], "dir")) {
+			linkdir(tokens[2], tokens[1]);
 		}
-		if (!strcmp(line_tokens[0], "files")) {
-			link_dotfiles(line_tokens[2], line_tokens[1]);
+		if (!strcmp(tokens[0], "files")) {
+			link_dotfiles(tokens[2], tokens[1]);
 		}
-		if (!strcmp(line_tokens[0], "install")) {
-			install(line_tokens[1]);
+		if (!strcmp(tokens[0], "install")) {
+			install(tokens[1]);
 		}
-		if (!strcmp(line_tokens[0], "run")) {
-			memset(command, 0, COMMAND_SIZE);
-			for (i = 1; i < token_index; i++) {
-				strncomb(command, COMMAND_SIZE - 1, line_tokens[i], " ", NULL);
-			}
-			system(command);
+		if (!strcmp(tokens[0], "run")) {
+			command(tokens, t);
 		}
 	}
 	return 0;
 }
 
+/* expand tilde in src to HOME, place result in dest and return pointer to it */
+char *expandtilde(char *dest, char *src, int size)
+{
+	char *tmp;
+	int len;
+
+	len = strlen(src); /* get length of src */
+	/* add space for null character */
+	tmp = calloc(len + 1, 1); /* get zeroed memory of size len */
+	memset(dest, 0, size); /* zero dest */
+	strncpy(tmp, src, len); /* copy src to tmp */
+	memmove(tmp, tmp+1, len); /* omit first character */
+	/* expand tilde */
+	strncomb(dest, size - 1, getenv("HOME"), tmp, NULL);
+	free(tmp);
+	return dest;
+}
+
+/* strip trailing newline of string s and return pointer to s */
+char *stripnewline(char *s)
+{
+	s[strcspn(s, "\n")] = 0;
+	return s;
+}
+
 /* tokenize lines of config file and call parseline() on each line */
 int readconfig(FILE *config)
 {
-	char line_tokens[TOKENS][TOKEN_SIZE];
-	char line_buffer[LINE_BUFFER_SIZE];
-	char expanded_token[TOKEN_SIZE];
-	int token_index;
+	char line_buffer[LINE_BUFFER_SIZE]; /* holds line before being tokenized */
+	char tokens[TOKENS][TOKEN_SIZE];
+	char exptoken[TOKEN_SIZE]; /* new token produced by expanding certain characters to other strings */
 	char *token;
-	int line;
+	int l;   /* keep track of line number */
 	int err;
+	int t;   /* amount of tokens */
 
-	line = 1;
+	l = 1;
 	while ((fgets(line_buffer, LINE_BUFFER_SIZE, config) != NULL)) {
+		/* process line buffer before it is tokenized */
 		switch (line_buffer[0]) {
 			case '\n':
 				continue;
@@ -291,34 +336,35 @@ int readconfig(FILE *config)
 				fgets(line_buffer, LINE_BUFFER_SIZE, config);
 				continue;
 		}
-		line_buffer[strcspn(line_buffer, "\n")] = 0;
+		stripnewline(line_buffer);
 
-		token_index = 0;
+		/* begin tokenization of line buffer */
+		t = 0;
 		token = strtok(line_buffer, " ");
 		while (token != NULL) {
-			/* expand ~ to $HOME */
-			if (token[0] == '~') {
-				memset(expanded_token, 0, TOKEN_SIZE);
-				memmove(token, token+1, strlen(token));
-				strncomb(expanded_token, TOKEN_SIZE - 1, getenv("HOME"), token, NULL);
-				strncpy(line_tokens[token_index], expanded_token, TOKEN_SIZE);
-			} else if (token[0] == '#') {
+			switch (token[0]) {
+			case '~': /* expand tilde to $HOME */
+				expandtilde(exptoken, token, TOKEN_SIZE);
+				strncpy(tokens[t], exptoken, TOKEN_SIZE);
+			case '#': /* skip lines that begin with # (comments) */
 				break;
-			} else {
-				strncpy(line_tokens[token_index], token, TOKEN_SIZE);
+			default: /* otherwise copy the token to the token array */
+				strncpy(tokens[t], token, TOKEN_SIZE);
 			}
 			token = strtok(NULL, " ");
-			token_index++;
+			t++; /* new line (increment line) */
 		}
 
-		if ((err = (parseline(line_tokens, token_index))) < 0) {
+		/* handle parser errors */
+		if ((err = (parseline(tokens, t))) < 0) {
 			printf("failed to parse line %i in config (error code %i)\n",
-			       line, err);
+			       l, err);
 			return err;
 		}
 
+		/* reset line buffer and increment line number */
 		memset(line_buffer, 0, LINE_BUFFER_SIZE);
-		line++;
+		l++;
 	}
 	return 0;
 }
@@ -327,6 +373,8 @@ int main(int argc, char **argv)
 {
 	FILE *config;
 	int err;
+
+	/* handle errors: */
 
 	if ((err = parseargs(argc, argv)) < 0) {
 		printf("failed to parse arguments (error code %i)\n", err);
